93fff9c842d8247555d31982ea4836ceedd9c80a
Mohammed-Ali Garouj <mgarouj@e3r4p14.1337.ma>
2026-06-24 17:35:59 +0100

    push

--- JS00/ex05/functions-arguments-parameter.js
+++ JS00/ex05/functions-arguments-parameter.js
@@ -12,3 +12,19 @@ function findMax() {
 
 var maxValue = findMax(1, 123, 500, 115, 44, 88);
 console.log(maxValue); // Output: 500
+
+
+
+
+function findMin() {
+    let min = Infinity; // Initialize min to the largest possible number
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i]; // Update min if the current argument is smaller
+        }
+    }   
+    return min; // Return the minimum value found
+}
+
+var minValue = findMin(1, 123, 500, 115, 44, 88);
+console.log(minValue); // Output: 1
