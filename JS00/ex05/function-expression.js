ef47a34f245480ccbf5072b092efbef91fb1529c
Mohammed-Ali Garouj <mgarouj@e3r4p14.1337.ma>
2026-06-24 17:42:08 +0100

    push

--- JS00/ex05/function-expression.js
+++ JS00/ex05/function-expression.js
@@ -1,1 +1,15 @@
-function mut
+
+
+// standard function
+function mutiply(a, b){
+    return a * b;
+}
+
+
+
+// function expression
+const multyply = function(a, b) {
+    return a * b;
+};
+
+console.log(multyply);
