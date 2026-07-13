b319ea58082a7d4b30f590238763900a6110c5fe
mohammed ali <mohammedgarouj7@gmail.com>
2026-07-13 19:14:29 +0100

    ss

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert('hello from file of js')
+let value = true;
+console.log(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+console.log(typeof value); // string
