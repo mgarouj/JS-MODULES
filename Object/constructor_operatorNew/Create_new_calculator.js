9e056525ec6a35b7fcbfa96cad6f90c2345ecc27
Mohammed-Ali Garouj <mgarouj@e3r10p14.1337.ma>
2026-07-17 19:08:46 +0100

    add something

---
+++ Object/constructor_operatorNew/Create_new_calculator.js
@@ -0,0 +1,13 @@
+
+function Calculator(){
+    read(){
+        
+    }
+}
+
+
+let calculator = new Calculator();
+calculator.read();
+
+console.log( "Sum=" + calculator.sum() );
+console.log( "Mul=" + calculator.mul() );
