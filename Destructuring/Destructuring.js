7c75d9ac59f768887be89bb8ce0ec101e7c104e3
mohammed ali <mohammedgarouj7@gmail.com>
2026-07-18 00:58:35 +0100

    add

--- Destructuring/Destructuring.js
+++ Destructuring/Destructuring.js
@@ -6,3 +6,13 @@ console.log(a, b, c);
 
 // instead of doing this the manual way
 // you unpack in one line. oreder matters position maps directly, like structured bindings
+
+let [first, , third] = [1, 2, 3];
+console.log(first, third);
+
+
+// rest pattern (grab the remainder)
+let [first, ...rest] = [1, 2, 3, 4];
+console.log(first); // 1
+console.log(rest); //[2, 3, 4]
+
