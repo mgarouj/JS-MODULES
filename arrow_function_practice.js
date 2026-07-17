c9faaa7dcaa61dc7669fab27bbd7524175d5a0d7
Mohammed-Ali Garouj <mgarouj@e3r10p14.1337.ma>
2026-07-17 15:55:18 +0100

    add some arrow function exemple

---
+++ arrow_function_practice.js
@@ -0,0 +1,14 @@
+let sum = (a, b) => a + b;
+
+console.log(sum(1, 3));
+
+
+let double = n => n * 2;
+
+console.log(double(4));
+
+let sayHi = () => console.log("hello from arrow function");
+sayHi();
+
+
+
