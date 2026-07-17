9d6ecac0d99a01b1d13f4ce3c378b9fba0a8cd79
Mohammed-Ali Garouj <mgarouj@e3r10p14.1337.ma>
2026-07-17 18:23:32 +0100

    add calculator task with object with this

---
+++ Object/tasks_this.js
@@ -0,0 +1,22 @@
+// calculator
+
+let calculator = {
+    a: 0,
+    b: 0,
+    read: function(a, b){
+        const readlineSync = require('readline-sync');
+
+        this.a = Number(readlineSync.question('Enter first number: '));
+        this.b = Number(readlineSync.question('Enter second number: '));
+    },
+    sum: function(){
+        return this.a + this.b;
+    },
+    mul: function(){
+        return this.a * this.b;
+    }
+}
+
+calculator.read();
+console.log( calculator.sum() );
+console.log( calculator.mul() );
