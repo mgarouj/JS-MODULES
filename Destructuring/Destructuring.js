793d187c403e7ffabf6a44939eae039173d32d1c
mohammed ali <mohammedgarouj7@gmail.com>
2026-07-18 00:52:32 +0100

    add

---
+++ Destructuring/Destructuring.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3];
+let [a, b, c] = arr;
+
+console.log(a, b, c);
+
+
+// instead of doing this the manual way
+// you unpack in one line. oreder matters position maps directly, like structured bindings
