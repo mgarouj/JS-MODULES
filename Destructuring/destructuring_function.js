676303c355c9d30825d9ad60492875eacafe5039
mohammed ali <mohammedgarouj7@gmail.com>
2026-07-18 01:22:47 +0100

    add

--- Destructuring/destructuring_function.js
+++ Destructuring/destructuring_function.js
@@ -1,3 +1,14 @@
+
+// whithout destructuring;
 function createUser(options){
-    let name = options.name
+    let name = options.name;
+    let age = options.age;
+    console.log(name, age);
 }
+
+// with destructuring - cleaner, and self-socumenting
+function createUser({ name, age }){
+    console.log(name, age);
+}
+
+createUser({ name: "Anna", age: 25});
