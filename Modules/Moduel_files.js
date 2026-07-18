9b059b653fa979b882cd980fc703d25a5cdf05a9
Mohammed-Ali Garouj <mgarouj@e3r10p12.1337.ma>
2026-07-18 13:03:39 +0100

    add something

---
+++ Modules/Moduel_files.js
@@ -0,0 +1,4 @@
+// a javascript module is usually a afile, but it can also be an html script. 
+// a module file is a .js file using import / export.
+// a module script is an html script using import /export
+
