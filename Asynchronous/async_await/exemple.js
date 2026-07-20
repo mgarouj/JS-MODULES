11a10c8f7b389e146f7bcbe667652ed05b767277
mohammed ali <mohammedgarouj7@gmail.com>
2026-07-20 11:35:48 +0100

    add some

--- Asynchronous/async_await/exemple.js
+++ Asynchronous/async_await/exemple.js
@@ -9,3 +9,22 @@ function wait(seconds) {
         }, seconds * 1000);
     });
 }
+
+async function main() {
+    console.log("Start");
+
+    try{
+        const result = await wait(2); // pauses here for 2 seconds
+        console.log(result);
+
+        const result2 = await wait(-1);
+        console.log(result2);
+    } catch (err) {
+        console.log("Caught an error:", err);
+    }
+
+    console.log("End");
+}
+
+main();
+console.log("This runs immediately, while main() is still waiting!");
