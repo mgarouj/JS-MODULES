5e9e0fbd70512b7c066c4fc8ada1f26a017691e1
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-23 08:38:55 +0100

    ss

--- JSON/index.js
+++ JSON/index.js
@@ -1,3 +1,5 @@
 JSON = (javascript object ntation) data-interchange Format 
         used for exchangin data between a server and a web application 
-        JSON files {Key:value} OR [{}, P{}, {}]
+        JSON files {Key:value} OR [{},  {}, {}]
+
+        JSON.stringify
