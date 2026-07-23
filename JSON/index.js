8b6c59277e1c9be0ef11f451cc04d49dc0077e5a
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-23 08:40:02 +0100

    ss

--- JSON/index.js
+++ JSON/index.js
@@ -1,6 +1,9 @@
-JSON = (javascript object ntation) data-interchange Format 
-        used for exchangin data between a server and a web application 
-        JSON files {Key:value} OR [{},  {}, {}]
+// JSON = (javascript object ntation) data-interchange Format 
+//         used for exchangin data between a server and a web application 
+//         JSON files {Key:value} OR [{},  {}, {}]
 
-        JSON.stringify() = converts a JS object to a JSON string.
-        JSON.parse() = converts a JSON string to a JS object
+//         JSON.stringify() = converts a JS object to a JSON string.
+//         JSON.parse() = converts a JSON string to a JS object
+
+
+
