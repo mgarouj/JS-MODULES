03ae0042c858dd6846ee031cd6f166ce02f95594
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-23 08:53:59 +0100

    ss

--- JSON/index.js
+++ JSON/index.js
@@ -33,7 +33,7 @@ const people = `[{
     "name":"Sandy",
     "age": 30,
     "isEmployed": false
-}];`
+}]`;
 
 // const jsonString = JSON.stringify(people);    this methos to convet a js object to an jsong string 
 
