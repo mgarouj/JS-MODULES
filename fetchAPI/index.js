ee0e7ee8588526854e2f41efd6f8065740010249
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-23 09:35:38 +0100

    ss

--- fetchAPI/index.js
+++ fetchAPI/index.js
@@ -1,3 +1,4 @@
 // fetch = Function used for making HTTP requests to fetch resources.
 //          (JSON style data, images, files)
 //          simplifies asynchronous data fetching in Javascript and 
+//          USED FOR INTERACTING WITH API
