376f1c3215c840900445d1c18ff00de52d88538c
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-23 08:39:08 +0100

    ss

--- JSON/index.js
+++ JSON/index.js
@@ -2,4 +2,4 @@ JSON = (javascript object ntation) data-interchange Format
         used for exchangin data between a server and a web application 
         JSON files {Key:value} OR [{},  {}, {}]
 
-        JSON.stringify
+        JSON.stringify() = converts a JS object 
