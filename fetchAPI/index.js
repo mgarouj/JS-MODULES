620c1b04cafad4871bd0675964ac2e3dbc76f694
Mohammed-Ali Garouj <mgarouj@e3r11p4.1337.ma>
2026-07-24 07:28:27 +0100

    ss

--- fetchAPI/index.js
+++ fetchAPI/index.js
@@ -5,7 +5,46 @@
 //          data asynchrounounsly over the web.
 //          fetch(url, {options})
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(respose => respose.json())
-    .then(data => console.log(data.name))
-    .catch(error => console.error(error));
+
+
+
+
+
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(respose => {
+//         if (!respose.ok){
+//             throw new Error("Could not fetch resource");
+//         }
+//         return respose.json();
+//     })
+//     .then(data => console.log(data.name))
+//     .catch(error => console.error(error));
+
+
+
+async function fetchData(){
+    try{
+
+        const pokemonNmae = document.getElementById("pokimonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNmae}`);
+
+        if (!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = 'block';
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
